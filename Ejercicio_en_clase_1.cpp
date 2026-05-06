f62aa88749de18fe2de3139582b0bb6fb3308caf
Kevin_Cast <Kevincastaneda274@gmail.com>
2026-05-06 11:54:58 -0500

    Integracion del main

--- Ejercicio_en_clase_1.cpp
+++ Ejercicio_en_clase_1.cpp
@@ -1,11 +1,31 @@
-#include <iostream>
+int main() {
+    // Arreglos separados (sin polimorfismo)
+    Producto* productos[2];
+    ProductoConDescuento* productosDesc[2];
 
+    // Productos normales
+    productos[0] = new Producto("Arroz", 3000, 3);
+    productos[1] = new Producto("Leche", 2500, 2);
 
-class car {
-    public:
-    int speed;
-    void accelerate () {
-        std::cout << "Velocidad:" << speed;
+    // Productos con descuento
+    productosDesc[0] = new ProductoConDescuento("Laptop", 2500000, 1, 0.10);
+    productosDesc[1] = new ProductoConDescuento("Celular", 1200000, 1, 0.15);
+
+    // Mostrar normales
+    for (int i = 0; i < 2; i++) {
+        productos[i]->mostrar();
     }
-};
 
+    // Mostrar con descuento
+    for (int i = 0; i < 2; i++) {
+        productosDesc[i]->mostrarConDescuento();
+    }
+
+    // Liberar memoria
+    for (int i = 0; i < 2; i++) {
+        delete productos[i];
+        delete productosDesc[i];
+    }
+
+    return 0;
+}
